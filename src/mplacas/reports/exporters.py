a3a227b38c847d14744888e003f2bdcd0bec96b9
Weliton Pinheiro <welitonsp@gmail.com>
2026-07-14 10:25:31 -0300

    refactor(reports): adicionar fachada pública dos exportadores

---
+++ src/mplacas/reports/exporters.py
@@ -0,0 +1,9 @@
+from mplacas.reports.pdf_exporter import PDF_MEDIA_TYPE, build_monthly_report_pdf
+from mplacas.reports.xlsx_exporter import XLSX_MEDIA_TYPE, build_monthly_report_xlsx
+
+__all__ = [
+    "PDF_MEDIA_TYPE",
+    "XLSX_MEDIA_TYPE",
+    "build_monthly_report_pdf",
+    "build_monthly_report_xlsx",
+]
